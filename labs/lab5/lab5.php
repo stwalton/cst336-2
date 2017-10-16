2b31373bec34275a4f5e8d26dc1ecba9d214813a
Steven Walton <stwalton@csumb.edu>
2017-10-16 00:34:33 +0000

    Lab 5 upload

--- labs/lab5/lab5.php
+++ labs/lab5/lab5.php
@@ -1,2 +1,39 @@
-<? php 
-include '../../../dbConnection.php'
+<!DOCTYPE html>
+<html> 
+    <head>
+        <title>Lab 5: PHP Admin/Device Search</title>
+    </head>
+    
+    <body> 
+        <h1> Technology Center: Checkout System</h1>
+        
+        <form>
+            Device: <input type="text" name="deviceName" placeholder="Device Name"/>
+            Type: 
+            <select name="deviceType">
+                <option>Select One</option>
+                <?=getDeviceTypes()?>
+            </select>
+            
+            <input type="checkbox" name="available">
+            <label for="available"> Available </label>
+            
+            <br>
+            Order by:
+            <input type="radio" name="orderBy" id="orderByName" value="name"/>
+            <label for="orderByName"> Name </label>
+            <input type="radio" name="orderBy" id="orderByPrice" value="price"/>
+            <label for="orderByPrice"> Price </label>
+            
+            <input type="Submit" value="Search!" name="submit">
+        </form>
+        
+        <hr>
+        
+        <?=displayDevices()?>
+        
+        <iframe name="checkoutHistory" width="400" height="400"></iframe>
+    </body>
+
+
+</html>
