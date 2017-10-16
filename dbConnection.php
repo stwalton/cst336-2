651c65c470b0e6a346cf680f41a8b38bf0e7d4ca
Steven Walton <stwalton@csumb.edu>
2017-10-16 06:06:54 +0000

    dbConnection fil

--- dbConnection.php
+++ dbConnection.php
@@ -1,24 +1,24 @@
 <?php
 
-function getDatabaseConnection() { 
-    $host = 'localhost';// cloud 9
-    $dbname = 'tcp';
-    $username = 'root';
-    $password = '';
+    function getDatabaseConnection() { 
+        $host = 'localhost'; // cloud 9
+        $dbname = 'tcp';
+        $username = 'root';
+        $password = '';
     
-    if (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
-        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-        $host = $url["host"];
-        $dbname = substr($url["user"], 1);
-        $username = $url["user"];
-        $password = $url["pass"];
+        if (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
+            $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+            $host = $url["host"];
+            $dbname = substr($url["user"], 1);
+            $username = $url["user"];
+            $password = $url["pass"];
+        }
+    
+        $dbConn = new PDO("mysql:host=$host; dbname=$dbneme", $username, $password);
+        $dbConn-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+        return $dbConn;
+    
     }
-    
-    $dbConn = new PDO("mysql:host=$host; dbname=$dbneme", $username, $password);
-    $dbConn-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-    return $dbConn;
-    
-}
 
 ?>
--- labs/lab5/lab5.php
+++ labs/lab5/lab5.php
@@ -1,50 +1,58 @@
 <?php
 
-include '../../dbConnection.php';
+    include '../../dbConnection.php';
 
-$conn = getDatabaseConnection();
+    $conn = getDatabaseConnection();
 
-function getDeviceTypes() {
-    global $conn;
-    $sql = "SELECT_DISTINCT(deviceType)
+    function getDeviceTypes() {
+        global $conn;
+        $sql = "SELECT_DISTINCT(deviceType)
             FROM 'tc_device'
             ORDER BY deviceType";
-    $stmt = $conn->prepare($sql);
-    $stmt->execute();
-    $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+        $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
     
-    foreach ($records as $record) {
-        echo "<option> " . $record['deviceType'] . "</option>";
+        foreach ($records as $record) {
+            echo "<option> " . $record['deviceType'] . "</option>";
+        }
     }
-}
 
-function displayDevices() {
-    global $conn;
-    $sql = "SELECT * FROM tc_device WHERE 1 ";
+    function displayDevices() {
+        global $conn;
+        $sql = "SELECT * FROM `tc_device WHERE 1 ";
     
-    if(isset($_GET['submit'])) {
-        $namedParameters = array();
+        if(isset($_GET['submit'])) {
+            $namedParameters = array();
         
-        if (!empty($_GET['deviceName'])) {
-            $sql .+ " AND deviceName Like :deviceName";
-            $namedParameters['deviceName'] = "%" . $_GET['deviceName'] . "%";
-        }
+            if (!empty($_GET['deviceName'])) {
+                $namedParameters[':deviceName'] = "%" . $_GET['deviceName'] . "%";
+                $sql .= " AND deviceName LIKE :deviceName";
+            }
         
-        if (!empty($_GET['deviceType'])) {
+            if (!empty($_GET['deviceType'])) {
+                $namedParameters[':deviceType'] = $_GET['deviceType'];
+                $sql .= " AND deviceType = :deviceType";
+            }
         
-        $sql .= " AND deviceType = :dType"; 
-        $namedParameters[':dType'] = $_GET['deviceType'];
-        
-        }
-        
-        if (isset($_GET['available'])) {
+            if (!empty($_GET['available'])) {
+                $namedParameters[':status'] = "A";
+                $sql .= " AND status = :status";
+            }
             
-        }
-    } //endIf (isset)
+            if (!empty($_GET['orderByName'])) {
+                $sql .= " AND status = :status";
+            } else if (!empty($_GET['orderByName'])) {
+                $sql .= " ORDER BY deviceName ASC";
+            } else {
+                $sql .= " ORDER BY deviceName";
+            }
+            
+        } //endIf (isset)
     
-    $stmt = $conn->prepare($sql);
-    $stmt->execute($namedParameters);
-    $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $stmt = $conn->prepare($sql);
+        $stmt->execute($namedParameters);
+        $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
     
         foreach ($records as $record) {
             echo $record['deviceName'] . " " . $record['deviceType'] . " " . 
@@ -52,7 +60,7 @@ function displayDevices() {
             "<a target='checkoutHistory' href='checkoutHistory.php?deviceId=".$record['deviceId']."'> Checkout History </a><br />";
         }
     
-}
+    }
 
 
 ?>
@@ -71,21 +79,22 @@ function displayDevices() {
             Device: <input type="text" name="deviceName" placeholder="Device Name"/>
             Type: 
             <select name="deviceType">
-                <option>Select One</option>
+                <option value="">Select One</option>
                 <?=getDeviceTypes()?>
             </select>
+            
+            <br />
+            Order by:
+            <input type="radio" name="orderBy" id="orderByName" value="name"/>
+             <label for="orderByName"> Name </label>
+            <input type="radio" name="orderBy" id="orderByPrice" value="price"/>
+             <label for="orderByPrice"> Price </label>
+            <br />
             
             <input type="checkbox" name="available">
             <label for="available"> Available </label>
             
-            <br>
-            Order by:
-            <input type="radio" name="orderBy" id="orderByName" value="name"/>
-            <label for="orderByName"> Name </label>
-            <input type="radio" name="orderBy" id="orderByPrice" value="price"/>
-            <label for="orderByPrice"> Price </label>
-            
-            <input type="Submit" value="Search!" name="submit">
+            <input class="Submit" type="submit" name="submit" value="Search!">
         </form>
         
         <hr>
