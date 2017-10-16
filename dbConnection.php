175dff3e54af84a5753b5565c038fe0d0a350134
Steven Walton <stwalton@csumb.edu>
2017-10-16 19:54:11 +0000

    Heroku link should connect to heroku db

--- dbConnection.php
+++ dbConnection.php
@@ -2,19 +2,19 @@
     //echo "Inside dbconnection";
     function getDatabaseConnection() { 
         echo "inside db connection function";
-        $host = 'localhost';//cloud 9
-        $dbname = 'tcp';
-        $username = 'root';
-        $password = '';
+        $host = 'us-cdbr-iron-east-05.cleardb.net';
+        $dbname = 'heroku_6426014b995b402';
+        $username = 'be6ab7d0f68ac3';
+        $password = '1c93f923';
     
-        if (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
+        //if (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
             //echo "if statement for heroku";
-            $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-            $host = $url["host"];
-            $dbname = substr($url["user"], 1);
-            $username = $url["user"];
-            $password = $url["pass"];
-       }
+            //$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+            //$host = $url["host"];
+            //$dbname = substr($url["user"], 1);
+            //$username = $url["user"];
+            //$password = $url["pass"];
+       //}
     
         $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
         $dbConn-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
