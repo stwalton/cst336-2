41f78afa2a2b3776f5e1c1ac1f4ccc79617212a9
Steven Walton <stwalton@csumb.edu>
2017-10-16 04:59:02 +0000

    dbConnection

--- labs/lab5/lab5.php
+++ labs/lab5/lab5.php
@@ -1,3 +1,63 @@
+<?php
+
+include '../../dbConnection.php';
+
+$conn = getDatabaseConnection();
+
+function getDeviceTypes() {
+    global $conn;
+    $sql = "SELECT_DISTINCT(deviceType)
+            FROM 'tc_device'
+            ORDER BY deviceType";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+    foreach ($records as $record) {
+        echo "<option> " . $record['deviceType'] . "</option>";
+    }
+}
+
+function displayDevices() {
+    global $conn;
+    $sql = "SELECT * FROM tc_device WHERE 1 ";
+    
+    if(isset($_GET['submit'])) {
+        $namedParameters = array();
+        
+        if (!empty($_GET['deviceName'])) {
+            $sql .+ " AND deviceName Like :deviceName";
+            $namedParameters['deviceName'] = "%" . $_GET['deviceName'] . "%";
+        }
+        
+        if (!empty($_GET['deviceType'])) {
+        
+        $sql .= " AND deviceType = :dType"; 
+        $namedParameters[':dType'] = $_GET['deviceType'];
+        
+        }
+        
+        if (isset($_GET['available'])) {
+            
+        }
+    } //endIf (isset)
+    
+    $stmt = $conn->prepare($sql);
+    $stmt->execute($namedParameters);
+    $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+        foreach ($records as $record) {
+            echo $record['deviceName'] . " " . $record['deviceType'] . " " . 
+            $record['price'] . " " . $record['status'] . 
+            "<a target='checkoutHistory' href='checkoutHistory.php?deviceId=".$record['deviceId']."'> Checkout History </a><br />";
+        }
+    
+}
+
+
+?>
+
+
 <!DOCTYPE html>
 <html> 
     <head>
