f85b198375c7db97216574e4c05955a38ac71c01
Steven Walton <stwalton@csumb.edu>
2017-10-16 22:24:46 +0000

    db connection; messed around with parameters

--- dbConnection.php
+++ dbConnection.php
@@ -1,20 +1,20 @@
 <?php
     //echo "Inside dbconnection";
     function getDatabaseConnection() { 
-        echo "inside db connection function";
-        $host = 'us-cdbr-iron-east-05.cleardb.net';
-        $dbname = 'heroku_6426014b995b402';
-        $username = 'be6ab7d0f68ac3';
-        $password = '1c93f923';
+        //echo "inside db connection function";
+        $host = 'localhost';
+        $dbname = 'tcp';
+        $username = 'root';
+        $password = '';
     
-        //if (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
+        if (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
             //echo "if statement for heroku";
-            //$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-            //$host = $url["host"];
-            //$dbname = substr($url["user"], 1);
-            //$username = $url["user"];
-            //$password = $url["pass"];
-       //}
+            $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+            $host = $url["host"];
+            $dbname = substr($url["user"], 1);
+            $username = $url["user"];
+            $password = $url["pass"];
+       }
     
         $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
         $dbConn-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
